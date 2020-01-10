0f8677d9aaa523a8369d3dd39b88147aed1edb3f
Pawan Grover <v-pagro@microsoft.com>
2020-01-10 00:57:06 -0800

    server side linking

--- src/ListTemplate.js
+++ src/ListTemplate.js
@@ -1,28 +1,38 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom";
 
-class ListTemplate extends Component{
-    constructor(props)
+class ListTemplate extends Component{    
+    constructor()
     {
-        super(props);
+        super();
+        this.state = {
+            data:[]
+        }
+        let data = [1,2,3,4,5];
+        data.map(rec=> rec)
+
+    }   
+     
+    componentDidMount(){
+        fetch("http://localhost:5000/ListThemes")
+        .then(results => results.json()) // do we need to return or data returns automatically?
+        .then(templates => {
+            this.setState({data: templates});
+            console.log('here is the data ' + this.state.data)
+        })
     }
 
-    list()
-    {
-        let data = [
-            {templateId:1 , templateName: 'Week-1' , templateTitle : "Spirit of Giving"},
-            {templateId:2 , templateName: 'Week-2' , templateTitle : "Meditate plenty"},
-            {templateId:3 , templateName: 'Week-3' , templateTitle : "Spiritiuality "},
-            {templateId:4 , templateName: 'Week-4' , templateTitle : "Spirit of Sprituality"},
-            {templateId:5 , templateName: 'Week-5' , templateTitle : "Giving"}
-        ]
-    return <ul>{data.map(function(rec){ let a = "/week/" + rec.templateId  ; return <li key={rec.templateId}><Link  to={a} >Week : {rec.templateId} Title {rec.templateTitle}</Link></li>})}</ul>;
-    }
-     
     render(){ 
-        return(   
-            <div>{this.list()}</div>                          
-        );
+        return(
+                <div>
+                    <h1> List of data </h1>
+                     {this.state.data.map(rec => {
+                         let a = "/week/" + rec.templateId;                        
+                        return (
+                                <li key={rec.templateId}><Link  to={a} >{rec.templateName} Title {rec.templateTitle}</Link></li>               
+                            )
+                     })}
+                </div>);
     }
 
 }
--- src/Week.js
+++ src/Week.js
@@ -3,14 +3,28 @@ import Slidediv from './Slidediv';
 import Videodiv from './Videodiv';
 class Week extends Component {   
     state = {
-        count:0
+        count:0,
+        data:[]
     }
      countChange(){ 
          this.setState({ count: this.state.count + 1 }) 
     }
- 
+    
+    componentDidMount(){
+        const { text, match: { params } } = this.props;
+        const { id } = params;
+        console.log("match " + this.props)
+        let url = "http://localhost:5000/ListWeek/"+id; 
+        fetch(url)
+        .then(results=>{
+            return results.json();
+        })
+        .then(res=>{
+            //console.log(res);
+            this.setState({data:res})
+        })
+    }
     render(){
-        debugger;
         let styles = {
             margin: '20px',
             width: '100%',
@@ -18,38 +32,46 @@ class Week extends Component {
             align: "center",
             backgroundColor: '#9aeaea',
           };
-            const { text, match: { params } } = this.props;
-            const { id } = params;
-            console.log("match " + this.props)
-            let sectionData = [
-                                {speaker:'',heading:'Welcome',sectionType:'div'},
-                                {speaker:'pawan1',heading:'h1',sectionType:'div'},
-                                {speaker:'pawan2',heading:'h2',sectionType:'vid',videoStart:10,videoEnd:20,videoURL:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
-                                {speaker:'pawan3',heading:'h3',sectionType:'div'},
-                                {speaker:'pawan4',heading:'h4',sectionType:'vid',videoStart:0,videoEnd:0,videoURL:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'},
-                            ];
+            // const { text, match: { params } } = this.props;
+            // const { id } = params;
+            // console.log("match " + this.props)
+            // let sectionData = [
+            //                     {speaker:'',heading:'Welcome',sectionType:'div'},
+            //                     {speaker:'pawan1',heading:'h1',sectionType:'div'},
+            //                     {speaker:'pawan2',heading:'h2',sectionType:'vid',videoStart:10,videoEnd:20,videoURL:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'},
+            //                     {speaker:'pawan3',heading:'h3',sectionType:'div'},
+            //                     {speaker:'pawan4',heading:'h4',sectionType:'vid',videoStart:0,videoEnd:0,videoURL:'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'},
+            //                 ];
+            let sectionData = this.state.data.find(rec=> {
+                console.log(rec.sectionId);
+                if (rec.sectionId==this.state.count+1) 
+                    return rec;
+            });
+            console.log(sectionData.sectionType)
+            // let rec = sectionData[0];
+          //  console.log("sectionData" + "Count " +this.state.count + " value " +  sectionData.speaker );
             let cntrl = "";
-            if(sectionData[this.state.count].sectionType=="div")  {
-                    cntrl = <Slidediv divStyle={styles}  id={this.state.count} 
-                    speaker={sectionData[this.state.count].speaker} 
-                    heading={sectionData[this.state.count].heading} />    
-            }                
-            else{
-                    let vUrl = sectionData[this.state.count].videoURL;
-                    if(sectionData[this.state.count].videoStart+sectionData[this.state.count].videoEnd>0)
-                        vUrl = sectionData[this.state.count].videoURL + "#t=" + sectionData[this.state.count].videoStart + ',' + sectionData[this.state.count].videoEnd;
-                    cntrl = <Videodiv   id={this.state.count} 
-                    videoURL={vUrl} 
-                    />
-                }
+            // if(sectionData[this.state.count].sectionType=="div")  {
+            //         cntrl = <Slidediv divStyle={styles}  id={this.state.count} 
+            //         speaker={sectionData[this.state.count].speaker} 
+            //         heading={sectionData[this.state.count].heading} />    
+            // }                
+            // else{
+            //         let vUrl = sectionData[this.state.count].videoURL;
+            //         if(sectionData[this.state.count].videoStart+sectionData[this.state.count].videoEnd>0)
+            //             vUrl = sectionData[this.state.count].videoURL + "#t=" + sectionData[this.state.count].videoStart + ',' + sectionData[this.state.count].videoEnd;
+            //         cntrl = <Videodiv   id={this.state.count} 
+            //         videoURL={vUrl} 
+            //         />
+            //     }
 
         return (
         <div id="mainDiv" >
             <button id="btn" onClick={()=>{this.countChange()}}>Click Me</button>        
             <h1>Slide Number : {this.state.count}</h1>
-            <h2>{id}</h2>
-            {cntrl}         
-                    
+            
+            {/* {cntrl}          */}
+              Hello      
         </div>
         )
         
